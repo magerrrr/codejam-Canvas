e22a4401fd1c23198fe274fad19a740e507bfab6
Kirill Mager <kiruha24@mail.ru>
2019-10-25 22:00:48 +0300

    feat: add 32*32 picture and picture from url

--- codejam-Canvas/script.js
+++ codejam-Canvas/script.js
@@ -1,8 +1,31 @@
-const url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+let url = '';
 let arr = [];
 
+const selectedCase = document.querySelector('.selectCase');
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+
+selectedCase.addEventListener("click", (event)=>{
+  console.log(event.target.innerHTML);
+  if(event.target.innerHTML === '4*4'){
+    url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
+    console.log('1');
+  } else if (event.target.innerHTML === '32*32'){
+    url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
+    console.log('2');
+  } else if (event.target.innerHTML === '256*256'){
+    url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png';
+    base_image = new Image();
+    base_image.src = url;
+    base_image.onload = function(){
+    ctx.drawImage(base_image, 0, 0, 512,512);
+  }
+    console.log('3');
+  }
+  getArr();
+})
+
+
 
 async function getArr(){
   await fetch(url)
@@ -22,15 +45,26 @@ async function getArr(){
 
     for( let i=0; i< arr.length; i++){
 
-      arr[i].forEach(val => {
-        console.log(val);
-        ctx.fillStyle = `#${val}`;
-        ctx.fillRect(x,y,pixelSize,pixelSize);
-        x +=pixelSize;
-      })
+      if(arr[0][0].length === 6){
+        console.log('первый случай');
+
+        arr[i].forEach(val => {
+          console.log(val);
+          ctx.fillStyle = `#${val}`; // # FFFFFF
+          ctx.fillRect(x,y,pixelSize,pixelSize);
+          x +=pixelSize;
+        })
+      } else if (arr[0][0].length === 4) {
+        console.log('второй');
+        arr[i].forEach(val => {
+          console.log(val);
+          ctx.fillStyle = `rgb(${val[0]}, ${val[1]}, ${val[2]}, ${val[3]})`; // rgb(x,y,z,t)
+          ctx.fillRect(x,y,pixelSize,pixelSize);
+          x +=pixelSize;
+        })
+      }
 
       x=0;
       y+=pixelSize;
   }
 }
-getArr();
