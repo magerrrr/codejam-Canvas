0420f48a4a90459e11e165faafe355591359644f
Kirill Mager <kiruha24@mail.ru>
2019-10-27 20:10:44 +0300

    refactor: delete all  console.log and make nice reading code

--- codejam-Canvas/script.js
+++ codejam-Canvas/script.js
@@ -6,34 +6,27 @@ let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
 
 selectedCase.addEventListener("click", (event)=>{
-  console.log(event.target.innerHTML);
   if(event.target.innerHTML === '4*4'){
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-
     url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/4x4.json';
-    console.log('1');
   } else if (event.target.innerHTML === '32*32'){
     url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/32x32.json';
-    console.log('2');
   } else if (event.target.innerHTML === '256*256'){
     url = 'https://raw.githubusercontent.com/rolling-scopes-school/tasks/master/tasks/stage-2/codejam-canvas/data/image.png';
     base_image = new Image();
     base_image.src = url;
+
     return base_image.onload = function(){
-     ctx.drawImage(base_image, 0, 0, 512,512);
+      ctx.drawImage(base_image, 0, 0, 512,512);
     }
-    console.log('3');
   }
   getArr();
 })
-
-
 
 async function getArr(){
   await fetch(url)
     .then(response => response.json() )
     .then(data => {
-
       data.forEach((elem, i) => {
       arr[i] = elem;
     })});
@@ -45,9 +38,7 @@ async function getArr(){
     let y = 0;
 
     for( let i=0; i< arr.length; i++){
-
       if(arr[0][0].length === 6){
-
         arr[i].forEach(val => {
           ctx.fillStyle = `#${val}`; // # FFFFFF
           ctx.fillRect(x,y,pixelSize,pixelSize);
@@ -66,6 +57,5 @@ async function getArr(){
       y+=pixelSize;
     }
 
-    //console.log(size);
-    arr = [];
-  }
+  arr = [];
+}
